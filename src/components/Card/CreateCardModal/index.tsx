ae55de1a70eb60a6cbe58fddc365818c0fd6fe9d
Gabriel Vargas <josegabrielrosariovarg@gmail.com>
2022-11-15 23:09:30 -0400

    get image from input

--- src/components/Card/CreateCardModal/index.tsx
+++ src/components/Card/CreateCardModal/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 import TimesButton from "../../TimesButton";
 import { useAppDispatch } from "../../../hooks/redux";
 import { closeModal } from "../../../store/reducers/modalSlice";
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 
 
@@ -35,11 +35,7 @@ const SelectDeckAndType = ({onChange}: {onChange: (event: React.ChangeEvent<HTML
 
 // ----------------------------------------------------
 
-const TextareaContainer = ({name, onChange, icons}: {
-    name: string,
-    onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
-    icons: JSX.Element | null 
-    }) => {
+const TextareaContainer = ({name, onChange, icons}: {name: string, onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,icons: JSX.Element | null }) => {
 
     return(
         <div className={styles.textareaContainer} >
@@ -49,26 +45,66 @@ const TextareaContainer = ({name, onChange, icons}: {
             </div>
             <textarea name={name.toLocaleLowerCase()} onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => onChange(event)} ></textarea>
         </div>
-    )
+    );
 }
 
 
 // ----------------------------------------------------
 
 
+const ImageInput = ({onUpload}: {onUpload: (sources: string[]) => any}) => {
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target!.files;
+        if(!files) return;
+
+        const imagesSrc: string[] = [];
+
+        for(let file of files){
+            if (file.type && !file.type.startsWith('image/')) {
+                console.log('File is not an image.', file.type, file);
+                return;
+            }
+    
+            const reader: FileReader = new FileReader();
+            reader.addEventListener('load', (event) => {
+                const result = reader.result as string;
+                if(result !== null) {
+                    imagesSrc.push(result);
+
+                    if(imagesSrc.length === files.length){
+                        onUpload(imagesSrc);
+                    }
+                }
+
+            })
+            reader.readAsDataURL(file);
+        }
+
+
+    }
+
+    return(
+        <label>
+            <input type="file" accept="image/jpeg, image/png, image/jpg" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleOnChange(event)} />
+            <AiFillFileImage fill="#fff" className={styles.icon} />
+        </label>
+    )
+}
+
+
 const CreateCardModal = () => {
     const dispatch = useAppDispatch();
-    const [state,setState] = useState({
+    const [state,setState] = useState<{type: string, deck: string, front: string, back: string}>({
         type: '',
         deck: '',
-        font: '',
+        front: '',
         back: ''
     });
 
     const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLSelectElement>) => {
         const key: string = event.target.name;
         const value: string = event.target.value;
-        console.log(key, ': ', value);
         setState({
             [key]: value,
             ...state
@@ -84,13 +120,12 @@ const CreateCardModal = () => {
             </div>
             <div className={styles.content} >
                 <SelectDeckAndType onChange={handleOnChange} />
-                <TextareaContainer onChange={handleOnChange} name="Front" icons={<AiFillFileImage fill="#fff" className={styles.icon} onClick={() => {}} />} />
-                <TextareaContainer onChange={handleOnChange} name="Back" icons={<AiFillFileImage  fill="#fff" className={styles.icon} onClick={() => {}} />} />
+                <TextareaContainer onChange={handleOnChange} name="Front" icons={<ImageInput onUpload={(sources: string[]) => {}} /> } />
+                <TextareaContainer onChange={handleOnChange} name="Back" icons={<ImageInput onUpload={(sources: string[]) => {}} />} />
             </div>
             <motion.button className={styles.createBtn} onClick={handleOnClick} >Create</motion.button>
         </div>
     )
 }
-
 
 export default CreateCardModal;
